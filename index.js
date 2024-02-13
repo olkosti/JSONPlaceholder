c021c8d45d362ea111fba50e3ebcdf217aeca060
unknown <kov1606@gmail.com>
2024-02-13 15:01:46 +0300

    корректировка по дополнениям в обратной связи

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const posts = document.querySelector('.posts');
+const API_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+function addPost (title, body) {
+    const divPost = document.createElement('div');
+    divPost.classList.add('post');
+    posts.appendChild(divPost)
+
+    const pTitle = document.createElement('p');
+    pTitle.classList.add('post__title');
+    pTitle.textContent = `Заголовок: ${title}`
+    divPost.appendChild(pTitle);
+
+    const pArticle = document.createElement('p');
+    pArticle.classList.add('post__article');
+    pArticle.textContent = `Статья: ${body}`
+    divPost.appendChild(pArticle);
+}
 
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch(API_URL)
 
     .then(resolve => resolve.json())
     .then((data) => {
         data.forEach(element => {
-            const divPost = document.createElement('div');
-            divPost.classList.add('post');
-            posts.appendChild(divPost)
-
-            const pTitle = document.createElement('p');
-            pTitle.classList.add('post__title');
-            pTitle.textContent = `Заголовок: ${element.title}`
-            divPost.appendChild(pTitle);
-
-            const pArticle = document.createElement('p');
-            pArticle.classList.add('post__article');
-            pArticle.textContent = `Статья: ${element.body}`
-            divPost.appendChild(pArticle);
-            });
-        })
+            addPost(element.title, element.body);
+        });
+    })
 
     .catch(err => console.log('Пpоизошла ошибка:', err))
 });
